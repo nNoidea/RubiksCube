2fe09fcdcce2803e04f48bca1082101f89081f5b
nNoidea <58337790+nNoidea@users.noreply.github.com>
2022-10-13 19:50:04 +0200

    Generated the whole cube's coordinates.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double[] myArray = { 0, 0, 0 };
-        Kubusje myCube = new Kubusje(myArray);
+        RubiksKubus cube = new RubiksKubus();
 
-        for (int i = 0; i < 6; i++){
-            System.out.print(Arrays.toString(myCube.planes[i].points[0]) + ",");
-            System.out.print(Arrays.toString(myCube.planes[i].points[1])+ ",");
-            System.out.print(Arrays.toString(myCube.planes[i].points[2])+ ",");
-            System.out.println(Arrays.toString(myCube.planes[i].points[3]));
+        for (int i = 0; i < 27; i++) {
+            for (int i2 = 0; i2 < 6; i2++) {
+                for (int i3 = 0; i3 < 4; i3++) {
+                    String endChar = ",";
+                    if (i3 == 3)
+                        endChar = "";
+
+                    System.out.print(Arrays.toString(cube.littleCube[i].planes[i2].points[i3]) + endChar);
+                }
+                System.out.print("\n");
+            }
+            System.out.print("\n");
         }
     }
 }
